80722cf0d229fcb3299c88bf021d7e38b0019858
Aidana B <dana12021996@gmail.com>
2021-05-25 19:29:22 -0400

    first commit in review1 branch

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -5,6 +5,9 @@ public class BranchingPractice {
         // enabled version control
         // added .gitignore with .idea , target folder and *.iml file
 
+        //reviewing the new branch
+
+
 
     }
 }
